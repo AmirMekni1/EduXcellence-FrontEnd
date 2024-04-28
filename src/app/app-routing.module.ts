59e640d88985a8ec6ec106506a7771d619a8e98e
AmirMekni1 <mekniamir09@gmail.com>
2024-04-28 17:58:14 +0100

    New

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './Accueil/main/main.component';
 import { Page404Component } from './Éléments supplémentaires/page-404/page-404.component';
 import { PageDErreurDeConnexionComponent } from './Éléments supplémentaires/page-d-erreur-de-connexion/page-d-erreur-de-connexion.component';
+import { DetaillsDesOffresComponent } from './Accueil/les-offres/detaills-des-offres/detaills-des-offres.component';
 
 const routes: Routes = [
   { path: "", component: MainComponent },
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: "Formateur", loadChildren: () => import("../app/Formateur/module-formateur/module-formateur.module").then((e) => e.ModuleFormateurModule) },
   { path : "Participant", loadChildren : () => import("../app/Participant/module-participant/module-participant.module").then((e)=> e.ModuleParticipantModule)},
   { path: "**", component: Page404Component },
-  { path: "404", component: PageDErreurDeConnexionComponent }
+  { path: "404", component: PageDErreurDeConnexionComponent },
+  { path: "DetailsOffres", component: DetaillsDesOffresComponent }
 ];
 
 @NgModule({
